cc94b3632f96f57e65776eab92780e37d7862484
Jun Yu <shockz@iquest.co.kr>
2017-07-04 18:31:21 +0900

    initialize solution

---
+++ sapHowmuchConsumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sapHowmuchConsumer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			Console.Write($"Press <Enter> to exit...");
+			Console.ReadLine();
+		}
+	}
+}
