c9a93b8d5275ce269032b1a71f4913f8206f83f2
Jun Yu <shockz@iquest.co.kr>
2017-07-07 14:16:52 +0900

    temp save

---
+++ sapHowmuchConsumer/DataClasses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sapHowmuchConsumer
+{
+	public class JournalEntry
+	{
+		public DateTime ReferenceDate { get; set; }
+		public DateTime DueDate { get; set; }
+		public DateTime TaxDate { get; set; }
+		public string Memo { get; set; }
+		public IEnumerable<JournalEntryLine> Lines { get; set; }
+	}
+
+	public class JournalEntryLine
+	{
+		public string AccountCode { get; set; }
+		public decimal Debit { get; set; }
+		public decimal Credit { get; set; }
+		public string ShortName { get; set; }
+		public string LineMemo { get; set; }
+		public DateTime DueDate { get; set; }
+		public DateTime ReferenceDate { get; set; }
+		public DateTime TaxDate { get; set; }
+		public decimal BaseSum { get; set; }
+	}
+}
---
+++ sapHowmuchConsumer/TaskExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sapHowmuchConsumer
+{
+	public static class TaskExtensions
+	{
+		public static object TryResult<T>(this Task<T> task)
+		{
+			if (task != null)
+			{
+				try
+				{
+					return task.Result;
+				}
+				catch
+				{
+					// ignore
+				}
+			}
+
+			return task;
+		}
+	}
+}
